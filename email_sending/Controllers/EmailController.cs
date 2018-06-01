006e828011a74e64b9dfe74b21a38ca28acd47d5
“Ivan <robince885@gmail.com>
2018-06-01 18:43:25 +0200

    Add example for mail sending

---
+++ email_sending/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+
+namespace email_sending.Controllers
+{
+    public class EmailController : ApiController
+    {
+        public void PostSendMail()
+        {
+            string subject = "Email Subject";
+            string body = "Email body";
+            string FromMail = ConfigurationManager.AppSettings["from"];
+            string emailTo = "ivan.vasiljevic@hotmail.com";
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient(ConfigurationManager.AppSettings["smtpServer"]);
+            mail.From = new MailAddress(FromMail);
+            mail.To.Add(emailTo);
+            mail.Subject = subject;
+            mail.Body = body;
+            SmtpServer.Port = int.Parse(ConfigurationManager.AppSettings["smtpPort"]);
+            SmtpServer.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["from"], ConfigurationManager.AppSettings["password"]);
+            SmtpServer.EnableSsl = bool.Parse(ConfigurationManager.AppSettings["smtpSsl"]);
+            SmtpServer.Send(mail);
+        }
+    }
+}
